c78e7cb7b541546dac14e482ebc55dcd1f44a917
lerfich <nikita.serg.12@gmail.com>
2023-04-15 21:23:02 +0300

    feat(server): 1 and 2 tasks

---
+++ task1.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+
+###################
+# ЧТОБЫ ЗАПУСТИТЬ, ДОСТАТОЧНО РАЗКОММЕНТИРОВАТЬ ОДИН ИЗ ПРИНТОВ В САМОМ НИЗУ
+###################
+
+urlDefault = "https://yandex.com.am/weather/?lat=46.20832443&lon=6.142743111" #погода в Женеве
+vkSpbuUrl = "https://vk.com/spb1724" #группа спбгу вк
+
+
+styleClass = "temp__value temp__value_with-unit" #стиль для тега, содержащего температуру (число)
+vkSpbuStyleClass = "wall_post_text" #стиль для тега, содержащего пост
+
+
+htmlTag = 'span' #типа тега для сайта погоды
+vkSpbuHtmlTag = 'div' #тип тега для вк
+
+
+#парсинг значения определенного тега со страницы
+def parseTagsTexts(url, styles, tag):
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+
+    tags = soup.findAll(tag, styles)
+    # for tag in tags:
+        # print(tag.text)
+    return tags[1].text
+
+# print(parseTagsTexts(vkSpbuUrl, vkSpbuStyleClass, vkSpbuHtmlTag), '\n\n ############################## \n\n Предпоследний пост в группе Спбгу ВК (https://vk.com/spb1724)')
+print("Погода в Женеве сейчас: ", parseTagsTexts(urlDefault, styleClass, htmlTag))
---
+++ task2.py
@@ -0,0 +1,32 @@
+import json
+import httplib2
+
+
+
+#сюда нужно вставить свой токен желательно, инструкуция в файле get_access
+access_token = "vk1.a.kak7aByTPqIBYnAMBYSK3P1f5DHYf9YA2hL0Ftjo9vBNK6COY2v92Hyrzo_GHgPjwUw_84n4Doi87ie2Zr4KI54ZODgqAzDPdht86I5fnF5fkfaQYAArjvk5LYcaer6cYVhtCSjANHtCN4iuaLzyErSBfxZQySEIUjyW40kIuzXBbtX6ehZDz-zY5w0WYf48Q-gjpTaUZeg7b_0_PPAyTg"
+
+
+###### Вместо слова wordToSearch можно вставить любое слово, частоту которого ты хочешь узнать в последних countPostsToFetch постах
+wordToSearch = 'спбгу'
+countPostsToFetch = 20 #Максимум 100
+
+
+get_spbu_vk_wall_url = f"https://api.vk.com/method/wall.get?v=5.131&owner_id=-52298374&count={countPostsToFetch}"
+http = httplib2.Http()
+res = http.request(get_spbu_vk_wall_url, method='POST', 
+                   headers={'Authorization': f'Bearer {access_token}'})
+
+
+all_posts = json.loads(res[1])
+all_posts = all_posts['response']['items']
+texts = [post['text'] for post in all_posts]
+
+def AverageCountWordInDocument(docs, word):
+    avgCount = []
+    for doc in docs:
+        avgCount.append(doc.upper().count(word.upper()))
+    return sum(avgCount)/len(docs)  
+
+
+print(AverageCountWordInDocument(texts, wordToSearch))
